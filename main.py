516047c0d898de057827d2d7e5debca5733b094d
yangxingchi <yang_xingchi@sina.cn>
2025-11-15 18:22:16 -0800

    add comment

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 from genetic_algorithm import ga
 from utils import *
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     results = {}
 
     for pop_size in pop_size_arr:
         results[pop_size] = []
+        print("-------- pop_size = {} ----------".format(pop_size))
         for generations in generation_arr:
             # print("-------- pop_size = {}, generation = {} ----------".format(pop_size, generations))
             best_solution, best_fitness, average_fitness = ga(pop_size, generations)
@@ -19,6 +19,7 @@ if __name__ == '__main__':
             # print("Best fitness:", best_fitness)
             # print("\n")
 
+    # generate plot
     for pop_size, data_list in results.items():
         plot_for_pop_size(pop_size, data_list)
 
